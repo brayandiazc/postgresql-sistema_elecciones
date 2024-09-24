e5538d9b3eabb294d204d2036b103ebcdc5dcaf6
Carlos Silva Diaz <crasd15@live.com>
2024-09-24 19:54:02 -0300

    creacion de tabla de votantes

--- schema.sql
+++ schema.sql
@@ -2,3 +2,10 @@
 CREATE DATABASE sistema_votacion;
 -- Conectar a la base de datos
 \c sistema_votacion;
+
+-- Crear la tabla de votantes
+CREATE TABLE Votantes (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100),
+  cedula VARCHAR(20) UNIQUE
+);
