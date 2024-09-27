b42c5f5718d4f3f5c6349f5cbe0057ccbd658985
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-27 17:25:02 -0500

    creada tabla de canidatos

---
+++ inserts_candidatos_5.sql
@@ -0,0 +1,6 @@
+INSERT INTO Candidatos (nombre, partido)
+VALUES ('Nicholas Masduro', 'PZUB'),
+  ('Juana Caino', 'Voluntad de Volar'),
+  ('Henri Casiles', 'First Justice'),
+  ('Marila Coina Macado', 'Vente Wenenzuela'),
+  ('Claud Fernin', 'Soluciones para Wenenzuela');
